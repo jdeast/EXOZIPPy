1d1f9ad5e418bc56df7df00cadc3b546082ca45c
jenniferyee <jennifer.c.yee@gmail.com>
2024-02-29 10:14:16 -0500

    Refactored into properties with lazy-loading rather than functions.

--- exozippy/ulens.py
+++ exozippy/ulens.py
@@ -22,47 +22,20 @@ class Phys2UlensConverter(object):
 
         # Probably all need to be replaced with private variables and
         # properties...
-        self.theta_E = None
-        self.pi_rel = None
-        self.mu_rel = None
+        self._theta_E = None
+        self._pi_rel = None
+        self._mu_rel = None
+        self._mu_rel_hel = None
+        self._v_earth_perp = None
 
-        self.t_E = None
-        self.rho = None
+        self._t_E = None
+        self._rho = None
         self.ulens_params = None
 
     def get_ulens_params(self):
-        self.calc_t_E()
-        self.calc_rho()
         self.ulens_params = {'t_E': self.t_E, 'rho': self.rho}
 
         return self.ulens_params
-
-    def calc_pi_rel(self):
-        self.pi_rel = self.lens.pi.value - self.source.pi.value
-
-    def calc_theta_E(self):
-        if self.pi_rel is None:
-            self.calc_pi_rel()
-
-        self.theta_E = np.sqrt(kappa * self.lens.mass * self.pi_rel)
-
-    def calc_t_E(self):
-        if self.theta_E is None:
-            self.calc_theta_E()
-
-        if self.mu_rel is None:
-            self.calc_mu_rel()
-
-        self.t_E = self.theta_E / self.mu_rel
-
-    def calc_rho(self):
-        if self.theta_E is None:
-            self.calc_theta_E()
-
-        self.rho = self.source.theta_star / self.theta_E
-
-    def calc_mu_rel(self):
-        pass
 
     @property
     def coords(self):
@@ -74,3 +47,47 @@ class Phys2UlensConverter(object):
     @coords.setter
     def coords(self, new_value):
         self._coords = mm.Coordinates(new_value)
+
+    @property
+    def pi_rel(self):
+        if self._pi_rel is None:
+            self._pi_rel = self.lens.pi.value - self.source.pi.value
+
+        return self.pi_rel
+
+    @property
+    def theta_E(self):
+        if self._theta_E is None:
+            self._theta_E = np.sqrt(kappa * self.lens.mass * self.pi_rel)
+
+        return self._theta_E
+
+    @property
+    def t_E(self):
+        if self._t_E is None:
+            self._t_E = self.theta_E / self.mu_rel.value
+
+        return self._t_E
+
+    @property
+    def rho(self):
+        if self._rho is None:
+            self._rho = self.source.theta_star / self.theta_E
+
+        return self._rho
+
+    @property
+    def mu_rel(self):
+        # mu_rel, hel = mu_rel + v_earth,perp * pi_rel / au
+        if self._mu_rel is None:
+            self._mu_rel = self.mu_rel_hel - self.v_earth_perp * self.pi_rel
+            # Units are going to be a thing...
+
+        return self._mu_rel
+
+    @property
+    def mu_rel_hel(self):
+        """
+        heliocentric lens-source relative proper motion vector
+        """
+        return self.lens.mu - self.source.mu
