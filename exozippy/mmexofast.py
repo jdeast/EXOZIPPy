ef8ece8f2491fd3b37a2ee69911d201de98c8073
jenniferyee <jennifer.c.yee@gmail.com>
2024-03-14 11:09:57 -0400

    Started sketching out initial fitting.

--- exozippy/mmexofast.py
+++ exozippy/mmexofast.py
@@ -4,6 +4,7 @@ High-level functions for fitting microlensing events.
 import os.path
 
 import MulensModel as mm
+import sfit_minimizer as sfit
 
 
 class MMEXOFASTSingleLensFitter():
@@ -41,6 +42,36 @@ class MMEXOFASTSingleLensFitter():
         Figure out initial values for the microlensing parameters
         :return:
         """
+        ef = mmexo.EventFinder(self.data) # architecture issue
+        ef.fit_grid()
+
+        init_sfit_params = mmexo.convert_ef_params(ef.best_grid_point)
+        event = mm.Event(datasets=self.data, model=mm.Model(init_sfit_params))
+
+        # Need to break this out into a separate function
+        parameters_to_fit = ['t_0', 'u_0', 't_E']
+        initial_guess = []
+        for key in parameters_to_fit:
+            if key == 't_E':
+                initial_guess.append(event.model.parameters.parameters[key].value)
+            else:
+                initial_guess.append(event.model.parameters.parameters[key])
+
+        for i in range(len(self.data)):
+            initial_guess.append(1.0)
+            initial_guess.append(0.0)
+
+        my_func = sfit.mm_funcs.PointLensSFitFunction(event, parameters_to_fit)
+
+        # Do the fit
+        result = sfit.minimize(
+            my_func, x0=initial_guess, tol=1e-5,
+            options={'step': 'adaptive'}, verbose=True)
+
+        # Probably want to convert to dict:
+        self.initial_mulens_parameters = result.x
+
+        # Need to repeat with FSEffects?
 
     def get_initial_physical_parameters(self):
         pass
