e589bf3b3628d3b1fa530ebbcf1edc683129bf86
jenniferyee <jennifer.c.yee@gmail.com>
2024-03-21 10:08:10 -0400

    Moved mu_S_geo to a prior.

--- examples/example_01_FSPL.py
+++ examples/example_01_FSPL.py
@@ -20,12 +20,14 @@ coords = "17:47:29.42, -34:43:35.6"
 
 expected = {'mulens': {'t_0': 5379.571, 'u_0': 0.523, 't_E': 17.94},
             'physical': {'D_L': 8.1, 'D_S': 9.7, 'M_L': 0.15,
-                     'mu_S_geo': [1.17, 0.25], 'mu_rel': 3.2,
+                    'mu_rel': 3.2,
                      'theta_E': 0.33}}
 # The physical parameters are NOT self-consistent. RP needs to fix this.
 # Also, we probably want a class for holding the parameters.
 
+priors = {'mu_S_geo': {'N': 1.17, 'E': 0.25}}
+
 results = mmexo.mmexofast.fit(
-    files=SAMPLE_FILE_01, coords=coords, fit_type='point lens',
+    files=SAMPLE_FILE_01, coords=coords, priors=priors, fit_type='point lens',
     print_results=True, verbose=True)
 print_metrics(results.final_parameters, expected)
