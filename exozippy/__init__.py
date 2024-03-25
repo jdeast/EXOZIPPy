7f33efb84810db0c96048685ce88b5d3589e3adf
jenniferyee <jennifer.c.yee@gmail.com>
2024-03-25 13:17:32 -0400

    Set up path to mulens datasets.

--- exozippy/__init__.py
+++ exozippy/__init__.py
@@ -1,1 +1,13 @@
+from os import path
 
+MODULE_PATH = path.abspath(__file__)
+for i in range(3):
+    MODULE_PATH = path.dirname(MODULE_PATH)
+
+path_1 = path.join(MODULE_PATH, 'data')
+if path.isdir(path_1):
+    DATA_PATH = path_1
+else:
+    DATA_PATH = path.join(path.dirname(__file__), 'data')
+
+MULENS_DATA_PATH = path.join(DATA_PATH, 'mulens')
