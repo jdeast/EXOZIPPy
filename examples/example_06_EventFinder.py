d2a73240ccb57aff9e9a20725fdbd94044f0d01b
jenniferyee <jennifer.c.yee@gmail.com>
2024-04-02 11:16:26 -0400

    Updated plots

--- examples/example_06_EventFinder.py
+++ examples/example_06_EventFinder.py
@@ -19,9 +19,9 @@ file_z087 = os.path.join(
     dir_, 'n20180816.Z087.WFIRST.{0:03}.txt'.format(lc_num))
 data_w149 = MulensModel.MulensData(file_name=file_w149, phot_fmt='flux')
 data_z087 = MulensModel.MulensData(file_name=file_z087, phot_fmt='flux')
-
+datasets = [data_w149, data_z087]
 # Do the EF search
-ef = mmexo.gridsearches.EventFinderGridSearch(datasets=[data_w149, data_z087])
+ef = mmexo.gridsearches.EventFinderGridSearch(datasets=datasets)
 ef.run(verbose=True)
 
 # Print best-fit parameters
@@ -30,22 +30,29 @@ print(ef.best)
 # Grid search chi2 plot with data (x2 for j=0, 1)
 plt.figure()
 for j in [1, 2]:
-    plt.subplot(2, 1, 1)
+    plt.subplot(1, 2, j)
     plt.title('j={0}'.format(j))
     plt.scatter(
-        ef.grid_t_0, ef.grid_t_eff, c=ef.results[:, j-1], edgecolors='black')
+        ef.grid_t_0, ef.grid_t_eff, c=ef.results[:, j-1]-ef.best['chi2'],
+        edgecolors='black', vmin=0, vmax=100, cmap='tab20b')
+    plt.colorbar('chi2')
+    plt.scatter(
+        ef.best['t_0'], ef.best['t_eff'], color='black', marker='x')
     plt.minorticks_on()
     plt.xlabel('t_0')
     plt.ylabel('t_eff')
 
 # Plot best-fit and best-fit for other j.
-best_model = mmexo.gridsearches.EFModel(ef.best)
-fit = MulensModel.FitData(dataset=data_w149, model=best_model)
-fit.fit_fluxes()
+best_model = mmexo.gridsearches.EFSFitFunction(data_w149, ef.best)
+best_model.update_all()
+
 plt.figure()
 data_w149.plot(phot_fmt='flux')
-best_model.plot_lc(t_range=[np.min(data_w149.time), np.max(data_w149.time)],
-    phot_fmt='mag', source_flux=fit.source_flux, blend_flux=fit.blend_flux,
-    color='black', lw=3)
+sorted = np.argsort(best_model.data[:, 0])
+plt.axvline(ef.best['t_0'], color='black')
+plt.axvline(ef.best['t_0'] - ef.best['t_eff'], color='black', linestyle='--')
+plt.axvline(ef.best['t_0'] + ef.best['t_eff'], color='black', linestyle='--')
+plt.plot(
+    best_model.data[sorted, 0], best_model.ymod[sorted], color='black')
 
 plt.show()
