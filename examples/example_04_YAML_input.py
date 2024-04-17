cb834e83d92f022973e739cf5dbdeacc10b1f06b
radek_poleski <radek.poleski@gmail.com>
2024-04-17 07:52:51 +0200

    ex04 - tabs replaced with spaces

--- examples/example_04_YAML_input.py
+++ examples/example_04_YAML_input.py
@@ -11,58 +11,58 @@ from print_ex_metrics import print_metrics
 
 
 def divide_settings(settings):
-	"""
-	divide settings into expected, files, and all others
-	"""
-	out = {**settings}
-	expected = out.pop('expected')
-	files_settings = out.pop('files')
+    """
+    divide settings into expected, files, and all others
+    """
+    out = {**settings}
+    expected = out.pop('expected')
+    files_settings = out.pop('files')
 
-	return (out, expected, files_settings)
+    return (out, expected, files_settings)
 
 
 def process_files(files, root=None, subdir=None):
-	"""
-	make a list with list of files
-	"""
-	if root is None:
-		root_path = ""
-	elif root == "MODULE_PATH":
-		root_path = mm.MODULE_PATH
-	else:
-		raise ValueError("root not recognized: " + str(root))
+    """
+    make a list with list of files based on root and subdir provided.
+    """
+    if root is None:
+        root_path = ""
+    elif root == "MODULE_PATH":
+        root_path = mm.MODULE_PATH
+    else:
+        raise ValueError("root not recognized: " + str(root))
 
-	if subdir is not None:
-		root_path = os.path.join(root_path, subdir)
+    if subdir is not None:
+        root_path = os.path.join(root_path, subdir)
 
-	if not isinstance(files, list):
-		raise TypeError('wrong value of files kwarg: ' + str(type(files)))
+    if not isinstance(files, list):
+        raise TypeError('wrong value of files kwarg: ' + str(type(files)))
 
-	out = []
-	for file_ in files:
- 		out.append(os.path.join(root_path, file_))
+    out = []
+    for file_ in files:
+        out.append(os.path.join(root_path, file_))
 
-	return out
+    return out
 
 
 if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		raise ValueError('single yaml file with settings is needed')
+    if len(sys.argv) != 2:
+        raise ValueError('single yaml file with settings is needed')
 
-	with open(sys.argv[1]) as in_file:
-		settings = yaml.safe_load(in_file)
+    with open(sys.argv[1]) as in_file:
+        settings = yaml.safe_load(in_file)
 
-	(kwargs, expected, file_settings) = divide_settings(settings)
+    (kwargs, expected, file_settings) = divide_settings(settings)
 
-	files = process_files(**file_settings)
-	kwargs['files'] = files
+    files = process_files(**file_settings)
+    kwargs['files'] = files
 
-	# u.M_e has to be parsed.
+    # u.M_e has to be parsed.
 
-	print(kwargs)
-	print()
-	print(expected)
+    print(kwargs)
+    print()
+    print(expected)
 
-	results = mmexo.mmexofast.fit(**kwargs)
+    results = mmexo.mmexofast.fit(**kwargs)
 
-	print_metrics(results.final_parameters, expected)
+    print_metrics(results.final_parameters, expected)
