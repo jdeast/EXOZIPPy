2ea44b30f6adb54b0be0f7abf73c30a9c54788bb
jenniferyee <jennifer.c.yee@gmail.com>
2024-06-03 11:14:41 -0400

    updated printing of binary_params to be more useful.

--- exozippy/mmexofast/mmexofast.py
+++ exozippy/mmexofast/mmexofast.py
@@ -119,8 +119,8 @@ class MMEXOFASTFitter():
             self.binary_params = self.get_initial_2L1S_params()
             if self.verbose:
                 print(
-                    'Initial 2L1S', self.binary_params.ulens,
-                    self.binary_params.mag_method)
+                    'Initial 2L1S params', self.binary_params.ulens)
+                print('mag_methods', self.binary_params.mag_method)
 
             # Do the full MMEXOFAST fit to get physical parameters
             self.results = self.do_mmexofast_fit()
