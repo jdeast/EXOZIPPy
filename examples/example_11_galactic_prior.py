4ea8978a5d809cdaa062bfe3cc8cd5c30baa37fa
jenniferyee <jennifer.c.yee@gmail.com>
2024-09-18 09:11:53 -0400

    Use Case for Galactic Model Priors/Penalty.

---
+++ examples/example_11_galactic_prior.py
@@ -0,0 +1,20 @@
+"""
+Use case for the architecture of the Galactic Model Penalty.
+"""
+import exozippy as mmexo
+
+my_star = mmexo.Star(
+    mass=1.0,  # M_Sun
+    distance=6.0,  # kpc
+    gal_coords=[0.0, -0.5],  # (ell, b) deg
+    mu_hel=[],  # mas/yr
+    extinction={'I': 1.5},  # A_I in mag
+    age=8.0  # Gyr
+)
+
+print('Galactic Model chi^2 Penalty: ', my_star.get_galactic_model_penalty())
+print('Probability of being a DISK star: ', my_star.disk_probability)
+print('Probability of being a BULGE star: ', my_star.bulge_probability)
+
+# Print a dictionary with the penalties from different parts of the models.
+print(my_star.galactic_model_penalty_components)
