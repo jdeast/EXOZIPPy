Fixed sign error.

examples/use_cases/example_05_RomanDC18.py
@@ -35,7 +35,7 @@ for file_ in files:
     elements = file_.split('.')
     lc_nums.append(int(elements[-2]))
 
-for lc_num in np.sort(lc_nums)[0:10]:
+for lc_num in np.sort(lc_nums):
     print('\n...Fitting light curve {0}...'.format(lc_num))
     try:
         results = fit_lc(lc_num, verbose=True)

exozippy/mmexofast/estimate_params.py
@@ -80,7 +80,8 @@ def get_wide_params(params):
     tau = (params['t_pl'] - params['t_0']) / params['t_E']
     u = np.sqrt(params['u_0']**2 + tau**2)
     s = 0.5 * (np.sqrt(u**2 + 4) + u)
-    alpha = np.arctan2(-params['u_0'], tau)
+    #alpha = np.arctan2(-params['u_0'], tau)
+    alpha = np.pi - np.arctan2(params['u_0'], tau)
     rho = params['dt'] / params['t_E'] / 2.
     q = 0.5 * params['dmag'] * (rho**2)
    