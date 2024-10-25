e095d87d62ed7f39fc0ccfe2adde10fea0b77048
jenniferyee <jennifer.c.yee@gmail.com>
2024-10-25 13:43:42 -0400

    Fixed path issues in example.

--- examples/use_cases/example_05_RomanDC18.py
+++ examples/use_cases/example_05_RomanDC18.py
@@ -15,8 +15,8 @@ def fit_lc(lc_num, verbose=False):
     results = exozippy.mmexofast.fit(
         files=[data.file_w149, data.file_z087], coords=data.coords, fit_type='binary lens',
         print_results=True, verbose=verbose,
-        output_file=os.path.join(
-            dir_, 'temp_output', 'WFIRST.{0:03f}.csv'.format(lc_num))
+        log_file=os.path.join(
+            exozippy.MODULE_PATH, 'EXOZIPPy', 'DC18Test', 'temp_output', 'WFIRST.{0:03f}.log'.format(lc_num))
     )
 
     return results
@@ -35,7 +35,7 @@ for file_ in files:
     elements = file_.split('.')
     lc_nums.append(int(elements[-2]))
 
-for lc_num in np.sort(lc_nums):
+for lc_num in np.sort(lc_nums[0:1]):
     print('\n...Fitting light curve {0}...'.format(lc_num))
     try:
         results = fit_lc(lc_num, verbose=True)
