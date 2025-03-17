0f25f1b17afa4426bf43727a04c6b0a7e19bb4c1
jenniferyee <jennifer.c.yee@gmail.com>
2025-03-17 17:11:32 -0400

    cleaned up code.

--- examples/use_cases/use_case_01_pymc.py
+++ examples/use_cases/use_case_01_pymc.py
@@ -24,27 +24,11 @@ from exozippy import MULENS_DATA_PATH
 # Affects choice of mm.Event vs. mm.FitData as the input.
 def ln_prob_mm(t_0, u_0, t_E, err, time, flux):
     """ combines likelihood and priors"""
-    #parameters = ['t_0', 'u_0', 't_E']
-    #model_parameters = {}
-
-    #for parameter, item in zip(parameters, inputs[0:len(parameters)]):
-    #    model_parameters[parameter] = item.item()
-    #
-    #datasets = []
-    #for i in range(int((len(inputs) - len(parameters)) / 3)):
-    #    datasets.append(mm.MulensData(
-    #        [inputs[len(parameters) + 3 * i], inputs[len(parameters) + 3 * i + 1], inputs[len(parameters) + 3 * i + 2]],
-    #        phot_fmt='flux'))
-
     model_parameters = {'t_0': t_0.item(), 'u_0': u_0.item(), 't_E': t_E.item()}
     datasets = [mm.MulensData([time, flux, err], phot_fmt='flux')]
 
     model = mm.Model(model_parameters)
     event = mm.Event(model=model, datasets=datasets)
-    #event.fit_fluxes()
-    #print(time[0:10])
-    #print(event.get_chi2(), t_0, u_0, t_E, event.get_ref_fluxes())
-    #print(event)
 
     chi2s = np.array(event.get_chi2_per_point()).transpose().squeeze()
 
@@ -56,62 +40,6 @@ class LogLike(Op):
     Op documentation: https://pytensor.readthedocs.io/en/latest/extending/op.html
     """
 
-    def get_data_tensors(self, datasets):
-        """
-        pymc Data containers:
-        https://www.pymc.io/projects/examples/en/latest/fundamentals/data_container.html
-
-        See example about WHO data on baby length.
-
-        Input:
-            datasets = *list* of *MulensModel.MulensData* objects
-
-        Output:
-            tensor_list = *list* with each component of each dataset converted to a pymc.Data object.
-
-            JCY: not sure if this is okay or if the data need to be converted to pytensors, specifically. i.e.,
-            Is pt.as_tensor() different from pymc.Data() in a meaningful way?
-
-        """
-        if isinstance(datasets, mm.MulensData):
-            datasets = [datasets]
-        elif not isinstance(datasets, list):
-            raise TypeError('datasets must be a *list* not', type(datasets))
-
-        tensor_list = []
-        for i, dataset in enumerate(datasets):
-            if not isinstance(dataset, mm.MulensData):
-                raise TypeError(
-                    'dataset {0} must be MulensModel.MulensData object, not {1}'.format(i,  type(dataset)))
-
-            # pm.Data(name, vector) didn't work
-            tensor_list.append(pytensor.as_symbolic(dataset.time, name='Time_{0}'.format(i)))
-            tensor_list.append(pytensor.as_symbolic(dataset.flux, name='Flux_{0}'.format(i)))
-            tensor_list.append(pytensor.as_symbolic(dataset.err_flux, name='Err_{0}'.format(i)))
-
-        return tensor_list
-
-    def get_parameter_tensors(self, model, theta, parameters_to_fit):
-        """
-        https://pytensor.readthedocs.io/en/latest/library/index.html
-        JCY: I'm thinking that as_symbolic replaced as_tensor at some point.
-
-        :param theta:
-        :param parameters_to_fit:
-        :return:
-        """
-
-        tensor_list = []
-        for i, parameter in enumerate(parameters_to_fit):
-            tensor_list.append(pytensor.as_symbolic(theta[i], name=parameter))
-
-        for parameter in model.parameters.parameters.keys():
-            if parameter not in parameters_to_fit:
-                tensor_list.append(
-                    pt.constant(model.parameters.parameters[parameter], name=parameter))
-
-        return tensor_list
-
     def make_node(self, t_0, u_0, t_E, err, time, flux) -> Apply:
         """
 
@@ -121,8 +49,6 @@ class LogLike(Op):
         :return:
         """
         # Convert inputs to tensor variables
-        #parameter_tensors = self.get_parameter_tensors(event.model, theta, parameters_to_fit)
-        #data_tensors = self.get_data_tensors(event.datasets)
         t_0 = pt.as_tensor(t_0)
         u_0 = pt.as_tensor(u_0)
         t_E = pt.as_tensor(t_E)
@@ -137,10 +63,7 @@ class LogLike(Op):
         # with the same dimensions and same data type as data
         # If data must always be a vector, we could have hard-coded
         # outputs = [pt.vector()]
-        #print([item.type() for item in data_tensors if item.name[0:4] == 'Flux'])
-        # outputs = [item.type() for item in data_tensors if item.name[0:4] == 'Flux']
         outputs = [flux.type()]
-        # JCY: does this mean we need the likelihood for each individual datapoint?
 
         # Apply is an object that combines inputs, outputs and an Op (self)
         return Apply(self, inputs, outputs)
@@ -172,23 +95,9 @@ parameters_to_fit = ['t_0', 'u_0', 't_E']
 
 log_like = LogLike()
 
-#print('\n*** Test data to pytensor ***')
-#tensor_data = log_like.get_data_tensors(data)
-#for item in tensor_data:
-#    print(item)
-#    print(item.name, item.name[0:4], item.type)
-#
-#print('\n*** Test model to pytensor ***')
-#theta = [model.parameters.parameters[param] for param in parameters_to_fit]
-#print(theta)
-#tensor_params = log_like.get_parameter_tensors(model=model, theta=theta, parameters_to_fit=parameters_to_fit)
-#for item in tensor_params:
-#    print(item)
-
 print('\n*** Test model + data to pytensor: ***')
 node = log_like.make_node(
     model.parameters.t_0, model.parameters.u_0, model.parameters.t_E, data.err_flux, data.time, data.flux)
-    #theta=theta, parameters_to_fit=parameters_to_fit, event=event)
 print(node)
 
 print('\n*** Test full pytensor class: ***')
@@ -209,7 +118,7 @@ def custom_dist_loglike(flux, t_0, u_0, t_E, err, date):
 
 # use PyMC to sampler from log-likelihood
 with pm.Model() as no_grad_model:
-    # uniform priors on m and c
+    # uniform priors on t_0, u_0, t_E
     t_0 = pm.Uniform("t_0", lower=model.parameters.t_0 - 5., upper=model.parameters.t_0 + 5., initval=model.parameters.t_0)
     u_0 = pm.Uniform("u_0", lower=0.5, upper=1.5, initval=model.parameters.u_0)
     t_E = pm.Uniform("t_E", lower=10., upper=40., initval=model.parameters.t_E)
