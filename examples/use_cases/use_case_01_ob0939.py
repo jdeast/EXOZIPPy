e27b97c9ea3d4f8d6fe7e3dc07629484811a9f6f
jenniferyee <jennifer.c.yee@gmail.com>
2025-10-08 09:55:16 -0400

    Expanded use case to include desired outputs.

--- examples/use_cases/use_case_01_ob0939.py
+++ examples/use_cases/use_case_01_ob0939.py
@@ -3,6 +3,8 @@ Analyze the ground-based data for microlensing event OB140939
 """
 import exozippy
 import os.path
+import matplotlib.pyplot as plt
+
 
 data_file = os.path.join(
     exozippy.MULENS_DATA_PATH, 'OB140939', 'n20100310.I.OGLE.OB140939.txt')
@@ -11,3 +13,26 @@ fitter = exozippy.mmexofast.MMEXOFASTFitter(
     files=[data_file], fit_type='point lens', coords='17:47:12.25 -21:22:58.7',
     verbose=True)
 fitter.fit()
+
+"""
+Desired outputs:
+
+Microlensing:
+- Static PSPL model parameters & uncertainties, chi2
+- Parallax PSPL model parameters & uncertainties, chi2
+- Parallax contour plot
+
+EXOZIPPy:
+- Best parallax PSPL model parameters & uncertainties, chi2
+- Posteriors for physical parameters
+
+Note: all chi2s need to be on the same system.
+"""
+
+fitter.print_static_pspl_results()
+fitter.print_parallax_pspl_results()
+fitter.fit_parallax_grid(plot=True)
+fitter.print_final_model()
+fitter.plot_posteriors()
+
+plt.show()
