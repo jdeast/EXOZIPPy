3abb67bee140e854736caf3b217f786b7e204b24
jenniferyee <jennifer.c.yee@gmail.com>
2026-05-06 06:48:34 -0400

    Cleaned up old code.

--- exozippy/mmexofast/estimate_params.py
+++ exozippy/mmexofast/estimate_params.py
@@ -1003,52 +1003,6 @@ def get_close_params(params, q=None, rho=None):
 
     return estimator_upper.binary_params, estimator_lower.binary_params
 
-    #if q is None:
-    #    q = 0.0040
-    #
-    #tau = (params['t_pl'] - params['t_0']) / params['t_E']
-    #u = np.sqrt(params['u_0']**2 + tau**2)
-    #
-    #s = 0.5 * (np.sqrt(u**2 + 4) - u)
-    #
-    #eta_not = (q**0.5 / s) * (1 / (np.sqrt(1 + s**2)) + np.sqrt(1 - s**2))
-    #mu = np.arctan2(eta_not, (s - 1 / s) / (1 + q)) # correction for primary --> COM
-    #phi = np.arctan2(tau, params['u_0'])
-    #
-    #alpha1 = np.pi / 2 - mu - phi
-    #alpha2 = alpha1 + 2 * mu
-    #
-    #alpha1_deg = correct_alpha(-np.rad2deg(alpha1) + 180.)
-    #alpha2_deg = correct_alpha(-np.rad2deg(alpha2) + 180.)
-    #
-    #if 'dt' in params.keys():
-    #    rho = params['dt'] / params['t_E'] / 2.
-    #elif 'rho' not in params.keys():
-    #    rho = 0.001
-    #
-    #new_params1 = {'t_0': params['t_0'],
-    #            'u_0': params['u_0'],
-    #            't_E': params['t_E'],
-    #            's': s,
-    #            'q': q,
-    #            'rho': rho,
-    #            'alpha': alpha1_deg}
-    #
-    #new_params2 = {'t_0': params['t_0'],
-    #            'u_0': params['u_0'],
-    #            't_E': params['t_E'],
-    #            's': s,
-    #            'q': q,
-    #            'rho': rho,
-    #            'alpha': alpha2_deg}
-    #
-    #out1 = BinaryLensParams(new_params1)
-    #out2 = BinaryLensParams(new_params2)
-    #out1.set_mag_method(params)
-    #out2.set_mag_method(params)
-    #
-    #return out1, out2
-
 
 class CloseUpperPlanetParameterEstimator(WidePlanetParameterEstimator):
 
@@ -1171,9 +1125,6 @@ def model_pspl_mag_at_pl(params):
     return model1.get_magnification(params['t_pl'])
 
 
-# In[ ]:
-
-
 class BinarySourceParams():
     """
     A class for managing parameters related to binary source models. Derived from equation 2.5 from Gaudi 1998.
@@ -1215,9 +1166,6 @@ class BinarySourceParams():
         u_0_2 = params["dt"] / (12**0.5 * params["t_E"])
         e = params["dmag"] * u_0_2 * A1
         self.source_flux_ratio = e
-
-
-# In[ ]:
 
 
 def get_binary_source_params(params):
@@ -1293,7 +1241,7 @@ class AnomalyPropertyEstimator():
     def get_anom_type(self):
         n_pts = np.sum(self.anom_index)
         sigmas = np.sign(self.residuals) * np.sqrt(self.chi2s)
-        med, std = np.nanmedian(sigmas), np.nanstd(sigmas)
+        #med, std = np.nanmedian(sigmas), np.nanstd(sigmas)
         #print('sigma dist', med, std, np.percentile(sigmas, q=[0, 1, 2, 98, 99, 100]))
         #plt.figure()
         #plt.hist(sigmas, bins=int(n_pts/40))
@@ -1572,119 +1520,3 @@ class AnomalyPropertyEstimator():
                 self.sorted_times) + self.blend_flux
 
         return self._expected_model_fluxes
-
-#        self.mask_type = mask_type
-#
-#        self._refined_pspl_params = None
-#        self._masked_datasets = None
-#
-#    def set_datasets_with_anomaly_masked(self, n_mask=3, tol=0.3):
-#        """
-#        Mask points associated with the anomaly.
-#
-#        :param mask_type: *str*
-#            `t_eff' or `residuals'. If `t_eff' mask based on t_pl +- n_mask * t_eff. If `residuals', mask based on
-#            deviation from existing point lens fit.
-#
-#        :param n_mask: *int*
-#            Number of +- `t_eff' to mask. Only used with mask_type = `t_eff'.
-#
-#        :param tol: *float*
-#            Maximum allowed deviation from point-lens in sigma. Only used with mask_type = `residuals'.
-#
-#        creates self.masked_datasets = *list* of MulensModel.MulensData objects with bad points masked.
-#
-#        """
-#        masked_datasets = []
-#        for dataset in self.datasets:
-#            masked_datasets.append(copy.copy(dataset))
-#
-#        for dataset in masked_datasets:
-#            if self.mask_type == 't_eff':
-#                index = ((dataset.time >
-#                         self.af_results['t_0'] -
-#                         n_mask * self.af_results['t_eff']) &
-#                         (dataset.time <
-#                          self.af_results['t_0'] +
-#                          n_mask * self.af_results['t_eff']))
-#            elif self.mask_type == 'residuals':
-#                index = self.get_residuals_mask(dataset, tol=tol)
-#                print(np.sum(index))
-#            else:
-#                raise ValueError("mask_type must be one of ['t_eff', 'residuals']. Your value ", self.mask_type)
-#
-#            dataset.bad = index
-#
-#        self._masked_datasets = masked_datasets
-#
-#    def get_residuals_mask(self, dataset, tol=None, max_diff=1):
-#        fit = MulensModel.FitData(dataset=dataset, model=MulensModel.Model(self.pspl_params))
-#        fit.fit_fluxes()
-#        ind_pl = np.argmin(np.abs(dataset.time - self.af_results['t_0']))
-#
-#        res, err = fit.get_residuals(phot_fmt='mag')
-#        out_tol = np.argwhere(((np.abs(res) / err) > tol) & fit.dataset.good).flatten()
-#        print(out_tol)
-#        diff = np.ediff1d(out_tol)
-#
-#        start = np.argmin(np.abs(out_tol - ind_pl))
-#        first, last = 0, len(out_tol) - 1
-#        for i in range(start, 0, -1):
-#            if diff[i] <= max_diff:
-#                first = i
-#            else:
-#                break
-#
-#        for i in range(start, len(out_tol)):
-#            if diff[i] <= max_diff:
-#                last = i
-#            else:
-#                break
-#
-#        print(ind_pl, res[ind_pl])
-#        print(ind_pl in out_tol)
-#        print(first, last, len(out_tol))
-#        print(out_tol[first], out_tol[last], out_tol[last] - out_tol[first])
-#        mask = np.zeros(len(dataset.time), dtype=bool)
-#        mask[out_tol[first]:out_tol[last]+1] = True
-#
-#        return mask
-#
-#    def get_dmag(self):
-#        """
-#        Find the magnitude difference at t_pl (af_results['t_0'])
-#        :return: dmag: *float*
-#        """
-#        event = mm.Event(datasets=self.masked_datasets, model=mm.Model(self.refined_pspl_params))
-#        dmag = []
-#        event.fit_fluxes()
-#        for fit in event.fits:
-#            residuals, errors = fit.get_residuals(bad=True, phot_fmt='flux')
-#            sigma = residuals[fit.dataset.bad]**2 / errors[fit.dataset.bad]**2
-#            index = np.argmax(sigma)
-#            mag_residuals, mag_errs = fit.get_residuals(bad=True, phot_fmt='mag')
-#            dmag.append(mag_residuals[fit.dataset.bad][index])
-#
-#        max_ind = np.argmax(np.abs(dmag))
-#        return dmag[max_ind]
-#
-#    def update_pspl_model(self):
-#        fitter = mmexo.fitters.SFitFitter(datasets=self.datasets, initial_model=self.pspl_params)
-#        fitter.run()
-#        new_params = {key: fitter.best[key] for key in self.pspl_params.keys()}
-#        self._refined_pspl_params = new_params
-#
-#
-#    @property
-#    def refined_pspl_params(self):
-#        if self._refined_pspl_params is None:
-#            self.update_pspl_model()
-#
-#        return self._refined_pspl_params
-#
-#    @property
-#    def masked_datasets(self):
-#        if self._masked_datasets is None:
-#            self.set_datasets_with_anomaly_masked()
-#
-#        return self._masked_datasets
